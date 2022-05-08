18377f6e48361300065c617e402e8ebf605a4896
Dallaporta-Timote <timote.dallaporta@viacesi.fr>
2022-05-08 21:33:58 +0200

    Clean Token Manager

--- TokenRetreiver.cs
+++ TokenRetreiver.cs
@@ -1,44 +1,40 @@
 ﻿using BungieApiHelper.Auth;
 using System.Net;
 using System.Threading.Tasks;
+using System.Web;
+using System.IO;
 
 namespace BungieApiHelper {
     public class TokenRetreiver {
-        private HttpListener Listener;
+        private readonly HttpListener _listener;
         public TokenRetreiver() {
-            Listener = new HttpListener() {
-                Prefixes = { "http://localhost:80/guardianbagpack/" }
-            ,
-
+            _listener = new HttpListener() {
+                Prefixes = { Locator.Config.AppRedirectUrl }
             };
         }
         public async Task WaitLogin() {
-
-            Listener.Start();
-            var context = (await Listener.GetContextAsync());
+            _listener.Start();
+            var context = (await _listener.GetContextAsync());
             CookieCollection cookies = context.Request.Cookies;
             HttpListenerResponse response = context.Response;
             TokenManager.State = cookies["State"].Value;
             TokenManager.Code = cookies["Code"].Value;
             TokenManager.Token = new AuthResponse() {
-                access_token = System.Web.HttpUtility.UrlDecode(cookies["Bearer"].Value),
-                expires_in = int.Parse(System.Web.HttpUtility.UrlDecode(cookies["Bearer_Expire"].Value)),
-                refresh_expires_in = int.Parse(System.Web.HttpUtility.UrlDecode(cookies["Refresh_Expire"].Value)),
-                membership_id = System.Web.HttpUtility.UrlDecode(cookies["Membership_Id"].Value),
+                access_token = HttpUtility.UrlDecode(cookies["Bearer"].Value),
+                expires_in = int.Parse(HttpUtility.UrlDecode(cookies["Bearer_Expire"].Value)),
+                refresh_expires_in = int.Parse(.HttpUtility.UrlDecode(cookies["Refresh_Expire"].Value)),
+                membership_id = HttpUtility.UrlDecode(cookies["Membership_Id"].Value),
                 token_type = "Bearer",
-                refresh_token = System.Web.HttpUtility.UrlDecode(cookies["Refresh"].Value),
+                refresh_token = HttpUtility.UrlDecode(cookies["Refresh"].Value),
             };
-
-            string responseString = "<HTML><BODY>Connection Successfull you can close this window and s</BODY></HTML>";
+            string responseString = "<HTML><BODY>Connection Successfull you can close this window and return to the app</BODY></HTML>";
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
             response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
+            Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
             output.Close();
-            Listener.Stop();
-            Listener.Close();
+            _listener.Stop();
+            _listener.Close();
         }
     }
 }
